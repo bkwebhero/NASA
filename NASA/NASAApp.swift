08cec29afcd6e534146cfd9da8c5f121ffa36b4b
Benjamin Kelsey <bk@bkwebhero.com>
2022-02-08 19:15:32 -0600

    Initial Commit

---
+++ NASA/NASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAApp.swift
+//  NASA
+//
+//  Created by Benjamin Kelsey on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
