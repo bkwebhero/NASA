de3148607f97d96f4fcf58ce99d907d5a96a2ea4
Benjamin Kelsey <bk@bkwebhero.com>
2022-02-09 21:26:41 -0600

    Add query parameter for date in order to show previous photos of the day

--- NASA/Feature/POTD/UseCase/FetchPOTDUseCase.swift
+++ NASA/Feature/POTD/UseCase/FetchPOTDUseCase.swift
@@ -9,15 +9,29 @@ import Foundation
 import Combine
 
 protocol FetchPOTDUseCaseProtocol {
-    func invoke() -> AnyPublisher<PhotoOfTheDay, NetworkError>
+    func invoke(for date: Date) -> AnyPublisher<PhotoOfTheDay, NetworkError>
 }
 
 class FetchPOTDUseCase: FetchPOTDUseCaseProtocol {
     // TO-DO: Unit tests
-    func invoke() -> AnyPublisher<PhotoOfTheDay, NetworkError> {
-        return APIService()
-            .makeNetworkRequest(url: "https://api.nasa.gov/planetary/apod?api_key=C9Zu9vHMVFuLCqMUr81vSu47R0xEoqprvBqQ0hGB",
-                                responseType: PhotoOfTheDay.self)
+    func invoke(for date: Date) -> AnyPublisher<PhotoOfTheDay, NetworkError> {
+        if let url = constructURL(date: date) {
+            return APIService().makeNetworkRequest(url: url, responseType: PhotoOfTheDay.self)
+        } else {
+            return Result.Publisher(NetworkError.invalidURL).eraseToAnyPublisher()
+        }
+    }
+    
+    private func constructURL(date: Date) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.nasa.gov"
+        components.path = "/planetary/apod"
+        components.queryItems = [
+            URLQueryItem(name: "api_key", value: "C9Zu9vHMVFuLCqMUr81vSu47R0xEoqprvBqQ0hGB"),
+            URLQueryItem(name: "date", value: date.toString(format: "YYYY-MM-dd"))
+        ]
+        return components.url
     }
     
 }
